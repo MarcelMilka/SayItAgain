a02e12c162f0cb2a4874fbdc2f1ccb852951e2c5
Marcello <marcello.sw.dev@gmail.com>
2025-07-22 21:41:52 +0200

    feat: implement TopBar according to the UI design

--- scaffold/src/main/java/eu/project/topBar/screen/TopBar.kt
+++ scaffold/src/main/java/eu/project/topBar/screen/TopBar.kt
@@ -3,6 +3,7 @@ package eu.project.topBar.screen
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,13 +11,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import eu.project.common.TestTags
 import eu.project.topBar.model.TopBarViewState
+import eu.project.ui.R
+import eu.project.ui.components.buttons.iconButton
+import eu.project.ui.components.spacers.spacerH8
+import eu.project.ui.components.text.headlineMedium
 import eu.project.ui.dimensions.DividerThickness
 import eu.project.ui.dimensions.TopBarHeight
 import eu.project.ui.dimensions.WidgetPadding
@@ -24,13 +31,13 @@ import eu.project.ui.theme.Divider
 
 @Composable
 internal fun topBar(
-    topBarViewState: TopBarViewState
+    topBarViewState: TopBarViewState,
 ) {
-
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(bottom = WidgetPadding.dp),
+            .padding(bottom = WidgetPadding.dp)
+            .testTag(TestTags.TOP_BAR),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
         content = {
@@ -40,11 +47,34 @@ internal fun topBar(
                     .fillMaxWidth()
                     .padding(top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding())
                     .height(TopBarHeight.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically,
                 content = {
 
-                    Text(stringResource(topBarViewState.screenName))
+                    if (topBarViewState.showBackIcon) {
+
+                        iconButton(
+                            onClick = {},
+                            painter = painterResource(R.drawable.back),
+                            contentDescription = stringResource(R.string.back_icon_description),
+                            testTag = TestTags.TOP_BAR_ICON_BUTTON_BACK_ICON
+                        )
+
+                        spacerH8()
+
+                        headlineMedium(text = stringResource(topBarViewState.screenName))
+                    }
+
+                    Spacer(modifier = Modifier.weight(1f))
+
+                    if (topBarViewState.showInfoIcon) {
+
+                        iconButton(
+                            onClick = {},
+                            painter = painterResource(R.drawable.info),
+                            contentDescription = stringResource(R.string.info_icon_description),
+                            testTag = TestTags.TOP_BAR_ICON_BUTTON_INFO_ICON
+                        )
+                    }
                 }
             )
 
