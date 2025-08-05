8200c2ae5d71754b828d36a306a952fde8a6596a
Marcello <marcello.sw.dev@gmail.com>
2025-08-05 20:03:35 +0200

    feat: add logic to convert SavedWord into ExportableSavedWord and vice versa

---
+++ feature/saved/src/main/java/eu/project/saved/exportWords/model/ExportableSavedWord.kt
@@ -0,0 +1,25 @@
+package eu.project.saved.exportWords.model
+
+import eu.project.common.model.SavedWord
+import java.util.UUID
+
+internal data class ExportableSavedWord(
+    val uuid: UUID,
+    val word: String,
+    val language: String,
+    val toExport: Boolean = false
+)
+
+internal fun ExportableSavedWord.convertToModel(): SavedWord =
+    SavedWord(
+        uuid = this.uuid,
+        word = this.word,
+        language = this.language
+    )
+
+internal fun SavedWord.convertToExportable(): ExportableSavedWord =
+    ExportableSavedWord(
+        uuid = this.uuid,
+        word = this.word,
+        language = this.language
+    )
