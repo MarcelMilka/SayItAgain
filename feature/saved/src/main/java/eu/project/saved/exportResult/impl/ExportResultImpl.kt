0e3d6cc93130dde034f7edb78424f4d8d072d3ed
Marcello <marcello.sw.dev@gmail.com>
2025-08-30 23:31:23 +0200

    feat(exportResultImpl): implement integration with the view model

--- feature/saved/src/main/java/eu/project/saved/exportResult/impl/ExportResultImpl.kt
+++ feature/saved/src/main/java/eu/project/saved/exportResult/impl/ExportResultImpl.kt
@@ -1,11 +1,30 @@
 package eu.project.saved.exportResult.impl
 
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import eu.project.common.navigation.Navigation
+import eu.project.saved.exportResult.vm.ExportResultViewModel
 
 fun NavGraphBuilder.exportResultImpl(controller: NavHostController) {
 
-    composable<Navigation.Saved.ExportResultScreen> { backStackEntry -> }
+    composable<Navigation.Saved.ExportResultScreen> { backStackEntry ->
+
+        val viewModel = hiltViewModel<ExportResultViewModel>()
+
+        val exportSettingsSerialized = remember {
+            backStackEntry
+                .toRoute<Navigation.Saved.ExportResultScreen>()
+                .exportSettingsSerialized
+        }
+
+        LaunchedEffect(exportSettingsSerialized) {
+
+            viewModel.retrieveExportSettings(exportSettingsSerialized = exportSettingsSerialized)
+        }
+    }
 }
